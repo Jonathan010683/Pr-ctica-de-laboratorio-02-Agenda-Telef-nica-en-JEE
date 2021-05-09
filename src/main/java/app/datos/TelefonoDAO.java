7486cae7f6c741a58f5e9978168ee11e4ca2add6
Author Jonnathan Uzhca <tata-1995.yo@hotmail.com>
2021-05-09 17:44:52 -0500

    Vista de inicio y crear-cuenta creadas

--- src/main/java/app/datos/TelefonoDAO.java
+++ src/main/java/app/datos/TelefonoDAO.java
@@ -9,10 +9,13 @@ import app.modelos.Telefono;
 
 public class TelefonoDAO {
 
+	public TelefonoDAO() {
+	}
+	
 	public void agregar(Telefono telefono, String cedulaUsuario) {
 		String sql = "INSERT INTO telefonos(numero, tipo, operadora, usuario_cedula) VALUES('" + 
 				      telefono.getNumero() + "', '" + telefono.getTipo() + "', '" +
-				      telefono.getOperadora() + "', '" + cedulaUsuario + "');";
+				      telefono.getOperadora() + "', '" + cedulaUsuario + "')";
 		BaseDatos.ejecutarSentencia(sql);
 	}
 	
@@ -21,15 +24,15 @@ public class TelefonoDAO {
 					 "numero = '" + telefono.getNumero() + "'," +
 					 "tipo = '" + telefono.getTipo() + "'," + 
 					 "operadora = '" + telefono.getOperadora() + "' " +
-					 "WHERE codigo = " + telefono.getCodigo() + ";";
+					 "WHERE codigo = " + telefono.getCodigo() + "";
 		BaseDatos.ejecutarSentencia(sql);
 	}
 	
 	public Telefono buscar(Integer codigo) {
 		Telefono telefono = null;
-		String sql = "SELECT * from telefonos WHERE codigo = " + codigo + ";";
-		ResultSet resultado = BaseDatos.ejecutarConsulta(sql);
+		String sql = "SELECT * from telefonos WHERE codigo = " + codigo;
 		try {
+			ResultSet resultado = BaseDatos.ejecutarConsulta(sql);
 			if (resultado.next()) {
 				telefono = new Telefono();
 				telefono.setCodigo(resultado.getInt("codigo"));
@@ -39,7 +42,7 @@ public class TelefonoDAO {
 			}
 			resultado.close();
 		} catch (SQLException e) {
-			System.out.println("ERROR DB: Ocurrió un problema al buscar el telefono especificado.");
+			System.out.println("ERROR DB: Ha ocurrido un problema al buscar el telefono especificado.");
 			System.out.println(e.getCause());
 			e.printStackTrace();
 		}
@@ -47,38 +50,16 @@ public class TelefonoDAO {
 	}
 
 	public void eliminar(Integer codigo) {
-		String sql = "DELETE FROM telefonos WHERE codigo = " + codigo + ";";
+		String sql = "DELETE FROM telefonos WHERE codigo = " + codigo;
 		BaseDatos.ejecutarSentencia(sql);
-	}
-	
-	public List<Telefono> listar() {
-		List<Telefono> telefonos = new ArrayList<>();
-		String sql = "SELECT * FROM telefonos;";
-		ResultSet resultados = BaseDatos.ejecutarConsulta(sql);
-		try {
-			while (resultados.next()) {
-				Telefono telefono = new Telefono();
-				telefono.setCodigo(resultados.getInt("codigo"));
-				telefono.setNumero(resultados.getString("numero"));
-				telefono.setTipo(resultados.getString("tipo"));
-				telefono.setOperadora(resultados.getString("operadora"));
-				telefonos.add(telefono);
-			}
-			resultados.close();
-		} catch (SQLException e) {
-			System.out.println("ERROR DB: Ocurrió un problema al listar los telefonos.");
-			System.out.println(e.getCause());
-			e.printStackTrace();
-		}
-		return telefonos;
 	}
 	
 	public List<Telefono> listarConFiltro(String filtro) {
 		List<Telefono> telefonos = new ArrayList<>();
 		String sql = "SELECT * FROM telefonos WHERE numero = '" + filtro + "'" +
-					 "OR usuario_cedula = '" + filtro + "';";
-		ResultSet resultados = BaseDatos.ejecutarConsulta(sql);
+					 "OR usuario_cedula = '" + filtro + "'";
 		try {
+			ResultSet resultados = BaseDatos.ejecutarConsulta(sql);
 			while (resultados.next()) {
 				Telefono telefono = new Telefono();
 				telefono.setCodigo(resultados.getInt("codigo"));
@@ -89,7 +70,7 @@ public class TelefonoDAO {
 			}
 			resultados.close();
 		} catch (SQLException e) {
-			System.out.println("ERROR DB: Ocurrió un problema al listar los telefonos.");
+			System.out.println("ERROR DB: Ha ocurrido un problema al listar los telefonos.");
 			System.out.println(e.getCause());
 			e.printStackTrace();
 		}
--- src/main/java/app/datos/UsuarioDAO.java
+++ src/main/java/app/datos/UsuarioDAO.java
@@ -6,26 +6,31 @@ import app.modelos.Usuario;
 
 public class UsuarioDAO {
 
+	public UsuarioDAO() {
+	}
+	
 	public void agregar(Usuario usuario) {
 		String sql = "INSERT INTO usuarios(cedula, nombre, apellido, correo, contrasena) VALUES('" +
 					  usuario.getCedula() + "', '" + usuario.getNombre() + "', '" + 
 				      usuario.getApellido() + "', '" + usuario.getCorreo() + "', '" +
-					  usuario.getContrasena() + "');";
+					  usuario.getContrasena() + "')";
 		BaseDatos.ejecutarSentencia(sql);
 	}
 	
 	public Usuario buscar(String cedula) {
 		Usuario usuario = null;
-		String sql = "SELECT * FROM usuarios WHERE cedula = '" + cedula + "';";
-		ResultSet resultado = BaseDatos.ejecutarConsulta(sql);
+		String sql1 = "SELECT * FROM usuarios WHERE cedula = '" + cedula + "'";
 		try {
+			ResultSet resultado = BaseDatos.ejecutarConsulta(sql1);
 			if (resultado.next()) {
+				TelefonoDAO telefonoDAO = new TelefonoDAO();
 				usuario = new Usuario();
 				usuario.setCedula(cedula);
 				usuario.setNombre(resultado.getString("nombre"));
 				usuario.setApellido(resultado.getString("apellido"));
 				usuario.setCorreo(resultado.getString("correo"));
 				usuario.setContrasena(resultado.getString("contrasena"));
+				usuario.setTelefonos(telefonoDAO.listarConFiltro(cedula));
 			}
 			resultado.close();
 		} catch (SQLException e) {
